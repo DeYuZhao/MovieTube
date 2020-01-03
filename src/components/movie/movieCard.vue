c016103cf53e32d412e0c91e737d1e1c8f0c2062
陈远志 <chenyuanzhi@weli.cn>
2020-01-03 17:52:37 +0800

    登出接口调整

--- src/components/movie/movieCard.vue
+++ src/components/movie/movieCard.vue
@@ -2,8 +2,9 @@
     <a-card hoverable class="movieCard ant-col-xs-8 ant-col-lg-6 ant-col-xxl-4">
     <img
       alt="example"
-      :src="movie.covers"
+      :src="movie.cover"
       slot="cover"
+      referrerPolicy="no-referrer"
     />
     <a-card-meta :title="movie.title">
       <template slot="description"
